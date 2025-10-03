e33d78d2bc6f7d858477fa94a5555ae384fc9fac
chaitu-1219 <p.chaitu1219@gmail.com>
2025-10-03 21:53:36 +0530

    Add files via upload

---
+++ PH_levels.py
@@ -0,0 +1,9 @@
+ph = int(input('enter ph value : '))
+if ph > 7:
+  print('Basic')
+elif ph < 7:
+  print('Acidic')  
+elif ph == 7:
+  print('Neutral')
+else:
+  print('error have to enter 0-14 only')  
