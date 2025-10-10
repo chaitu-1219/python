b12b046870d82014821a130e6383c9d593c896b8
chaitu-1219 <p.chaitu1219@gmail.com>
2025-10-11 02:03:53 +0530

    Add files via upload

---
+++ Pin_enter.py
@@ -0,0 +1,9 @@
+print("BANK OF CHAITANYA")
+
+pin = int(input("Enter your PIN Number: "))
+
+while pin !=1106:
+  pin = int(input("Invalid PIN!Please enter Again: "))
+
+if pin == 1106:
+  print("Correct PIN")  
